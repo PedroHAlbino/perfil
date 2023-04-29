b73aae9d728a73550605e753cc9b6126ea25de62
PedroAlbino <pedro.albino@consulticom.com.br>
2023-04-29 10:54:04 -0300

    Teste

---
+++ lib/index.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:perfil/list.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(
+        child: ListView(
+          children: ListMenu.get(context),
+        ),
+      ),
+    );
+
+  }
+}
---
+++ lib/text_elevated_outlined_button.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TextElevated extends StatelessWidget {
+  const TextElevated({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
